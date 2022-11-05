9558ff55b0c5d199e4574f68f5f4420f7ac26521
Ryan Baertlein <rdaniel707@yahoo.com>
2022-11-05 14:42:27 -0600

    LinkedIn update & removed duplicator plugin

--- wp-content/plugins/duplicator/installer/dup-installer/templates/default/pages-parts/help/addtional-help.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-defined('ABSPATH') || defined('DUPXABSPATH') || exit;
-
-?>
-<div id="addtional-help-content">
-    For additional help please visit <a href="https://snapcreek.com/support/docs/" target="_blank">Duplicator Migration and Backup Online Help</a>
-</div>
